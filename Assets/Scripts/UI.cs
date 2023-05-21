aa8318b938b975c8de21ad6677f213029ce0f85c
Petra Gunnarsdotter <gunnarsdotter@live.com>
2023-05-21 22:16:08 +0200

    Add X (close) to topplist and function to view highscore from start menu.

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -9,39 +9,43 @@ public class UI : MonoBehaviour
    
     [SerializeField] private Transform scrollViewContent;
     [SerializeField] private GameObject prefab;
+    [SerializeField] private GameObject StartFrame;
+    [SerializeField] private GameObject HighScore;
     // Start is called before the first frame update
     void Start()
     {
-        loadHighscoores();
     }
     
     void loadHighscoores()
     {
-        //TODO skapa lista som sparas någonstans och som är sorterad. Med namn och  score
+        //TODO skapa lista som sparas någonstans och som är sorterad.
+        //#Med namn och  score
+        //Töm listan
         string[] names = { "Petra", "JAg", "ks", "dss" };
         for(int i  = 0; i < names.Length; i++) 
         {
             GameObject newText = Instantiate(prefab, scrollViewContent);
             newText.transform.Find("name").GetComponent<TMPro.TextMeshProUGUI>().text = i+1+". "+names[i];
-            newText.transform.Find("score").GetComponent<TMPro.TextMeshProUGUI>().text = 0;
+            newText.transform.Find("score").GetComponent<TMPro.TextMeshProUGUI>().text = "" + 0;
         }
     }
 
     public void OpenHighscoore()
     {
-        //Hide buttons
-        //Visa listan + text
+        StartFrame.gameObject.SetActive(false);
+        loadHighscoores();
+        HighScore.gameObject.SetActive(true);
     }
     public void CloseHighscoore()
     {
-        //Hide listan + text
-        //Visa buttons
+        StartFrame.gameObject.SetActive(true);
+        HighScore.gameObject.SetActive(false);
     }
 
     public void startGame(string scenname)
     {
         SceneManager.LoadScene(scenname);
-    }
+    } 
     public void endGame()
     {
         Application.Quit();
